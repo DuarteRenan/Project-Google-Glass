9acc328990809840d8c161a53ec315ae6a8d42e6
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2020-10-13 23:56:27 -0300

    Files Added
    Files added with annotations inside the codes and the initial version of the website.

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,12 @@
+/*function mudaFoto (foto){
+		document.getElementById('icone').src = foto;
+		document.getElementById('icone').style.filter = "opacity(100%)";
+	}*/
+
+function mudaFoto (foto){
+		document.getElementById('icone').src=foto;
+		this.icone.style.transitionDuration="1s"
+
+	}
+
+		
